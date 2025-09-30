fbc0ab8666a2e33617d268a2ab13dc5e5832baad
Your Name <you@example.com>
2025-09-30 15:51:18 -0300

    Estoy haciendo el CSS de Favoritos

--- Front-end/pantalla de finacilacion de pantalla/finalizacion de compra.js
+++ Front-end/pantalla de finacilacion de pantalla/finalizacion de compra.js
@@ -1,3 +1,19 @@
 function seleccionar(nombre) {
-    document.getElementById("seleccionado").innerHTML = `<img src="${rutaImagen}" alt="Medio de pago">`;
+    document.getElementById("seleccionado").innerHTML = `<img src="${nombre}" alt="Medio de pago">`;
 }
+let carrito = [
+    { nombre: "Cartuchera", precio: 3000, cantidad: 1 },
+    { nombre: "Lapicera", precio: 500, cantidad: 1 }
+  ];
+
+
+  function agregarProducto(nombre, precio, cantidad = 1) {
+      let productoExistente = carrito.find(p => p.nombre === nombre);
+
+
+      if (productoExistente) {
+          productoExistente.cantidad += cantidad;
+      } else {
+          carrito.push({ nombre, precio, cantidad});
+      }
+  }
