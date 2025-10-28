438b15aeb38f08c8b7d2a95197405a735f25469d
Your Name <you@example.com>
2025-10-28 14:28:52 -0300

    Segui con el css de inicio

--- Front-end/Inicio de sesion/Inicio de sesion.js
+++ Front-end/Inicio de sesion/Inicio de sesion.js
@@ -4,12 +4,15 @@ function ValidarDNI() {
     let Contraseña = document.getElementById("CajaDeContraseña").value;
     let MensajeDeError = document.getElementById("MensajeDeError")
 
+
 if (DNI !== "49961473" || Contraseña !== "Hola") {
    MensajeDeError.style.color = "red";
    MensajeDeError.textContent = "ERROR No se encontro el usuario. El DNI o contraseña puede ser incorrecta"
+
 
 } else {
     MensajeDeError.style.color = "green";
     MensajeDeError.textContent = "DNI correcto"
 }
 }
+
