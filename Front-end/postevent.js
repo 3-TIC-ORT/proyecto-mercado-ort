e89c8fc7ba3d46b931cde864a5caf511186ae2ca
vicenteverruno <50155297@est.ort.edu.ar>
2025-11-10 14:27:05 -0300

    torticommit

---
+++ Front-end/postevent.js
@@ -0,0 +1,10 @@
+connect2server(3000)
+let dobleplaga = document.getElementById("CajaDeNombre")
+let comida = document.getElementById("CajaDeApellido")
+let pelota = document.getElementById("CajaDeDNI")
+let lapicera = document.getElementById("CajaDeMail")
+let compu = document.getElementById("CajaDeContraseña")
+PostEvent("registracion" (dobleplaga, comida, pelota, lapicera,compu), () => {
+ if({ok:true})
+
+})
